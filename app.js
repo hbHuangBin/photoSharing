ef9080fb98cc05df3dffe1fb8bcd3e0d5b6d9e15
Nobel Huang <nobel1984@gmail.com>
2014-11-12 23:39:01 +0800

    Use busboy for multipart parsing

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var busboy = require('connect-busboy');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -20,6 +21,7 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(busboy());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
--- routes/upload.js
+++ routes/upload.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET upload page. */
 router.get('/', function(req, res) {
-  res.render('pjax/upload');
+	res.render('pjax/upload');
 });
 
 module.exports = router;
